9784fde9c96884489811d370fb7a02bc1a37ae34
Naveen Chulani <nchulani2@gmail.com>
2019-07-07 17:34:36 -0500

    added preloader

--- src/index.js
+++ src/index.js
@@ -2,14 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 
+const loader = document.querySelector('.loader');
+
 class Index extends React.Component {
+  hideLoader = () => {
+    loader.classList.add('loader--hide');
+  };
+
+  showLoader = () => {
+    loader.classList.remove('loader--hide');
+  };
+  componentDidMount = () => {
+    this.hideLoader();
+  };
   render() {
     return (
-      <div className="Index">
+      <div className="Index animated fadeIn slow delay-0s">
         <App />
       </div>
     );
   }
 }
 
-ReactDOM.render(<Index />, document.querySelector('#root'));
+setTimeout(() => {
+  ReactDOM.render(<Index />, document.querySelector('#root'));
+}, 1000);
